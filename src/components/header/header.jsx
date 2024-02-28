79ab629db3f34dce86e6b1ac84bfb40d086f6dec
David Martz <d86.martz@gmail.com>
2024-02-28 20:11:53 +0100

    Update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,21 @@
 // import headerNav from "../navBar/navBar";
-
+import Image from "../../assets/images/logo-210w.webp";
 const Header = () => {
   return (
     <header>
-      <img
-        src="../../assets/images/logo-210w.webp"
-        srcset="../../assets/images/logo-150w.webp, ../../assets/images/logo-210w.webp"
-        sizes="(max-width:600px) 150px, 210px"
-        alt="Logo de la société"
-        classname="header-logo"
-      />
-      <div>jusque la ça marche</div>
+      {Image}
+      <div class="header-logo">
+        <img
+          src={"../../assets/images/logo-210w.webp"}
+          srcset={"../../assets/images/logo-150w.webp 150w, ../../assets/images/logo-210w.webp 210w"}
+          sizes="(max-width:600px) 150px, 210px"
+          alt="Logo de la société"
+          classname="header-logo"
+        />
+      </div>
       {/* <headerNav /> */}
     </header>
   );
 };
+
 export default Header;
